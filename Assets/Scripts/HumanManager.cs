8573a6ae3857ba2982a28c266cfaeba99265d721
nakano yosuke <naninuneno0411@gmail.com>
2018-07-08 22:01:10 +0900

    add HumanManager

---
+++ Assets/Scripts/HumanManager.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HumanManager : MonoBehaviour {
+    [SerializeField]
+    SimpleAnimation humanAnimation;
+
+
+	void Start () {
+        humanAnimation.Play("Run");
+    }
+    
+    void Update () {
+		
+	}
+}
